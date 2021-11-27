8e4f82af679b73f70d63913c4a984d79d0d4d1db
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-27 01:10:52 -0300

    feat: template method pattern

---
+++ template/taxes.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+
+# abstract taxes
+class ConditionalTaxTemplate(ABC):
+    def calculate(self, budget):
+        if self.should_max_tax(budget):
+            return self.max_tax(budget)
+        return self.min_tax(budget)
+
+    @abstractmethod
+    def should_max_tax(self):
+        pass
+
+    @abstractmethod
+    def max_tax(self):
+        pass
+
+    @abstractmethod
+    def min_tax(self):
+        pass
+
+# base classes
+class ICMS:
+    def calculate(self, budget):
+        return budget.amount * 0.1
+
+class ISS:
+    def calculate(self, budget):
+        return budget.amount * 0.6
+
+
+# conditional taxes
+class ICPP(ConditionalTaxTemplate):
+    def should_max_tax(self, budget):
+        return budget.amount > 500
+
+    def max_tax(self, budget):
+        return budget.amount * 0.07
+
+    def min_tax(self, budget):
+        return budget.amount * 0.05
+
+class IKCV(ConditionalTaxTemplate):
+    def should_max_tax(self, budget):
+        return budget.amount > 500 and any(
+            budget.amount > 100 for budget in budget.get_items())
+
+    def max_tax(self, budget):
+        return budget.amount * 0.1
+
+    def min_tax(self, budget):
+        return budget.amount * 0.06
